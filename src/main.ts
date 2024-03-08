50da80d1daaea09d1ea446058659a44c91bc5ebd
Alexander <desousa.alexander@gmail.com>
2024-03-08 12:31:02 -0400

    test swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Sport API')
+  .setDescription('It is an API used by the betting site')
+  .setVersion('1.0')
+  .addTag('sportapi')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 30000);
 }
