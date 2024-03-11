6ca36d84218a21512fb1ad05f5978906a5409873
Alexander <desousa.alexander@gmail.com>
2024-03-11 11:19:03 -0400

    GetAnonActiveLeagues

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,19 @@ async function bootstrap() {
   .setTitle('Sport API')
   .setDescription('It is an API used by the betting site')
   .setVersion('1.0')
-  .addTag('sportapi')
+  .addBearerAuth()
   .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const options = {
+    customfavIcon: '<path>/favicon.png', //adding our favicon to swagger
+    customSiteTitle: 'Sport API Docs', //add site title to swagger for nice SEO
+    swaggerOptions: {
+      persistAuthorization: true, // this helps to retain the token even after refreshing the (swagger UI web page)
+      // swaggerOptions: { defaultModelsExpandDepth: -1 } //uncomment this line to stop seeing the schema on swagger ui
+    },
+  };
+  SwaggerModule.setup('api', app, document, options);
   
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 30000);
 }
--- src/resources/dgs-proxy/player/player.controller.ts
+++ src/resources/dgs-proxy/player/player.controller.ts
@@ -2,8 +2,17 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PlayerService } from './player.service';
 import { CreatePlayerDto } from './dto/create-player.dto';
 import { UpdatePlayerDto } from './dto/update-player.dto';
+import {
+  ApiBasicAuth,
+  ApiInternalServerErrorResponse,
+  ApiNotAcceptableResponse,
+  ApiNotFoundResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
 
 @Controller('proxy/player')
+@ApiTags('proxyPlayer')
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
