247bf4476a8ae5f102bef0e7c2284af5a636c39e
Alexander <desousa.alexander@gmail.com>
2024-03-18 16:52:36 -0400

    multiple db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './config/database/database.module';
 import { ResourcesModule } from './resources/resources.module';
 import { SharedModule } from './common/shared.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { mongoConfig } from './config/database/mongo';
+import { DgsConfig } from './config/database/mssql';
 
-
+console.log(DgsConfig)
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.prod.env', '.test.env', '.env'], // Cargamos los ficheros de .env
+      envFilePath: ['.env'], // Cargamos los ficheros de .env
+      isGlobal: true,
     }), 
-    
-                // Inyectamos TypeOrme
-                // TypeOrmModule.forRoot(
-                //   {
-                //   type: 'mssql',
-                //   host: process.env.MSSQL_DATABASE_HOST,
-                //   port: Number(process.env.MSSQL_DATABASE_PORT) || 1433,
-                //   username: process.env.MSSQL_DATABASE_USER,
-                //   password: process.env.MSSQL_DATABASE_PASSWORD,
-                //   database: process.env.MSSQL_DATABASE_NAME,
-                //   requestTimeout: 5000,
-                //   options: {
-                //     encrypt: false, // Disable SSL/TLS
-                //   },
-                //   entities: [`${__dirname}/**/*.entity{.ts,.js}`], // se cargan todas las entidades de la base de datos
-                //   synchronize: false, //process.env.NODE_ENV === 'development', // Sincronizar la base de datos si estamos en entorno de desarrollo
-                //   logging: process.env.NODE_ENV === 'development' ? 'all' : false, // si esta en modo desarrollo, se muestra los logs
-                // }
-                // ),
-    DatabaseModule,
+
+    // // Inyectamos TypeOrme
+    TypeOrmModule.forRoot(DgsConfig),
+    TypeOrmModule.forRoot(mongoConfig),
     ResourcesModule,
     SharedModule,
     ThrottlerModule.forRoot([
