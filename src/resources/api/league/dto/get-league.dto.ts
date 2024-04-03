582fb1398953e6c6f76f227c217edcd6f2bf3601
Alexander <desousa.alexander@gmail.com>
2024-04-03 12:41:49 -0400

    Add SP custom [VZ_GetActiveLeagues]

--- src/resources/api/league/dto/get-league.dto.ts
+++ src/resources/api/league/dto/get-league.dto.ts
@@ -3,9 +3,19 @@ import { IsNotEmpty, IsNumber } from 'class-validator';
 import { toNumber } from '../../../../helpers/cast.helper';
 
 
-export class FindByBookQuery {
+export class GetActiveLeaguesQuery {
   @Transform(({ value }) => toNumber(value, { min: 1 }))
   @IsNotEmpty()
   @IsNumber()
   book_id: number;
+
+  @Transform(({ value }) => toNumber(value, { min: 0 }))
+  @IsNotEmpty()
+  @IsNumber()
+  line_type_id: number;
+
+  @Transform(({ value }) => toNumber(value, { min: 0 }))
+  @IsNotEmpty()
+  @IsNumber()
+  lang_id: number;
 }
