12d94bf7a3e3fb4b4f07f88a4ee9328b284e2eea
Alexander <desousa.alexander@gmail.com>
2024-04-04 14:32:16 -0400

    Add Player Login

--- src/resources/api/player/player.controller.ts
+++ src/resources/api/player/player.controller.ts
@@ -1,9 +1,9 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
   Post,
-  Query,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
@@ -11,6 +11,7 @@ import { TransformInterceptor } from '../../../interceptor/transform.interceptor
 import { ApiKeyAuth } from '../../../decorator/auth.decorator';
 import { IdPlayerParam } from './dto/get_player.dto';
 import { PlayerService } from './player.service';
+import { LoginParams } from './dto/login.dto';
 
 @Controller('player')
 @ApiKeyAuth()
@@ -18,11 +19,11 @@ import { PlayerService } from './player.service';
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
-  // @Post()
-  // @UseInterceptors(TransformInterceptor)
-  // async login(@Query() params: LoginParams): Promise<string> {
-  //   return await this.playerService.login(params);
-  // }
+  @Post('login')
+  @UseInterceptors(TransformInterceptor)
+  async login(@Body() params: LoginParams): Promise<string> {
+    return await this.playerService.login(params);
+  }
 
   @Get(':player_id/balance')
   @UseInterceptors(TransformInterceptor)
@@ -37,13 +38,3 @@ export class PlayerController {
     return await this.playerService.getInfo(params);
   }
 }
-
-// exec GetPlayerForLogin @UserName='DWP-10'
-// go
-// declare @p6 int
-// set @p6=377544
-// exec CreateCall @IdPlayer=21595,@PhoneLine=-1,@IdUser=0,@IP='0.0.0.0',@System='I',@IdCall=@p6 output,@URL=''
-// select @p6
-// go
-// exec WebGetPlayerOnline @IdPlayer=21595
-// go
--- src/resources/api/player/player.service.ts
+++ src/resources/api/player/player.service.ts
@@ -1,4 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Player } from './entities/player.entity';
 import { Repository } from 'typeorm';
@@ -18,7 +23,99 @@ export class PlayerService {
 
   */
   async login(params: any) {
-    return '';
+    let user = params.user;
+    let password = params.password;
+    let book_id = params.book_id;
+    let ip = params.ip;
+
+    const player: any = Object.values(
+      await this.getPlayerByUserName({ user }),
+    )[0];
+
+    if (!player) {
+      throw new NotFoundException(`Invalid user ${user}. Not found`);
+    } else if (player.OnlineAccess != 1 || player.IdBook != book_id) {
+      throw new UnauthorizedException(
+        `Player ${user} doesnt have Online Access. Contact Customer Services.`,
+      );
+    } else if (player.UserName != user || player.OnlinePassword != password) {
+      throw new UnauthorizedException(`Invalid user name or password.`);
+    }
+
+    let call = await this.createCallInDGS({ player_id: player.IdPlayer, ip });
+    let info = await this.getInfo({ player_id: player.IdPlayer });
+    info[0].IdCall = call[0].IdCall;
+    return info;
+  }
+
+  // exec GetPlayerForLogin @UserName='DWP-10'
+  // go
+  // declare @p6 int
+  // set @p6=377544
+  // exec CreateCall @IdPlayer=21595, @PhoneLine=-1, @IdUser=0, @IP='0.0.0.0', @System='I', @IdCall=@p6 output, @URL=''
+  // select IdCall = @p6
+  // go
+  // exec WebGetPlayerOnline @IdPlayer=21595
+  // go
+
+  /**
+    EXEC [CreateCall]
+    @IdPlayer int,
+    @PhoneLine smallint,
+    @IdUser smallint,
+    @IP varchar(100),
+    @System char(1),
+    @IdCall int OUTPUT,
+    @URL varchar(50) = null
+  */
+  async createCallInDGS(params: any) {
+    let cacheTimeSec = 1;
+    let player_id = params.player_id;
+    let ip = params.ip;
+
+    // **CHECK CACHE
+    const key = `get_createCallInDGS_${player_id}`;
+    const cached = await this.cacheService.get(key);
+    if (cached) return cached;
+    // **CHECK CACHE
+
+    const data = await this.playerRepository.query(
+      ` declare @p6 int;
+        EXEC CreateCall ${player_id}, 1, 0, '${ip}', 'I', @p6 output, '';
+        select IdCall = @p6
+        `,
+    );
+
+    // **SET CACHE
+    await this.cacheService.set(key, data, cacheTimeSec * 1000);
+    // **SET CACHE
+
+    return data;
+  }
+
+  /**
+    EXEC [VZ_GetPlayerBalance]
+    @prmIdPlayer int
+  */
+  async getPlayerByUserName(params: any) {
+    let cacheTimeSec = 1;
+    let user = params.user;
+
+    // **CHECK CACHE
+    const key = `get_playerByUserName_${user}`;
+    const cached = await this.cacheService.get(key);
+    if (cached) return cached;
+    // **CHECK CACHE
+
+    const data = await this.playerRepository.query(
+      `EXEC GetPlayerForLogin	'${user}'`,
+    );
+
+    // **SET CACHE
+    await this.cacheService.set(key, data, cacheTimeSec * 1000);
+    // **SET CACHE
+
+    return data;
   }
 
   /**
@@ -44,8 +141,10 @@ export class PlayerService {
 
     return data;
   }
-  /**
 
+  /**
+    EXEC [VZ_GetPlayerInfo]
+    @prmIdPlayer int
   */
   async getInfo(params: any) {
     let cacheTimeSec = 2;
