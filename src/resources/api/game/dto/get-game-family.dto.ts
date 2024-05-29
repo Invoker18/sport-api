13771410e5664521a442806e8aa42384a75c2adc
Alexander <desousa.alexander@gmail.com>
2024-05-29 16:07:48 -0400

    Add Period in Endpoint Games

--- src/resources/api/game/dto/get-game-family.dto.ts
+++ src/resources/api/game/dto/get-game-family.dto.ts
@@ -1,5 +1,11 @@
 import { Transform } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { toNumber } from '../../../../helpers/cast.helper';
 
 export class GetFamilyGamesQuery {
@@ -17,4 +23,9 @@ export class GetFamilyGamesQuery {
   @IsNotEmpty()
   @IsNumber()
   player_id: number;
+
+  @Transform(({ value }) => toNumber(value, { min: -1 }))
+  @IsOptional()
+  @IsNumber()
+  period?: number = -1;
 }
