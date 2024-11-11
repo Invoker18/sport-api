0b8d5688c90ccd85f8f1e2ba9db6cc95e8a2dad2
Alexander <desousa.alexander@gmail.com>
2024-11-11 13:13:40 -0400

    Add timezones and language

--- src/resources/api/player/player.service.ts
+++ src/resources/api/player/player.service.ts
@@ -247,4 +247,40 @@ export class PlayerService {
 
     return data;
   }
+
+  async getLanguages() {
+    const cacheTimeSec = 30;
+
+    // **CHECK CACHE
+    const key = `get_languageCultureInfo`;
+    const cached = await this.cacheService.get(key);
+    if (cached) return cached;
+    // **CHECK CACHE
+
+    const data = this.playerRepository.query(`EXEC WebGetLanguageCultureInfo`);
+
+    // **SET CACHE
+    await this.cacheService.set(key, data, cacheTimeSec * 1000);
+    // **SET CACHE
+
+    return data;
+  }
+
+  async getTimeZones() {
+    const cacheTimeSec = 30;
+
+    // **CHECK CACHE
+    const key = `get_languageCultureInfo`;
+    const cached = await this.cacheService.get(key);
+    if (cached) return cached;
+    // **CHECK CACHE
+
+    const data = this.playerRepository.query(`EXEC WebGetLanguageCultureInfo`);
+
+    // **SET CACHE
+    await this.cacheService.set(key, data, cacheTimeSec * 1000);
+    // **SET CACHE
+
+    return data;
+  }
 }
