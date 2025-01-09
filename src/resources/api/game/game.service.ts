e23d249c9442ee1f0b9d6fbd860be7c77e1af229
Alexander <desousa.alexander@gmail.com>
2025-01-09 14:24:16 -0400

    Fixed in search

--- src/resources/api/game/game.service.ts
+++ src/resources/api/game/game.service.ts
@@ -37,15 +37,16 @@ export class GameService {
     const game_ids = params.game_ids;
     const player_id = params.player_id;
     const lang_id = params.lang_id;
-    const player = await this.player.getInfo({ player_id: player_id });
-    const agent_id = player.IdAgent;
-    const line_type_id = player.IdLineType;
 
     // **CHECK CACHE
     const key = `get_game_by_game_ids_${game_ids}_${player_id}_${lang_id}`;
     const cached = await this.cacheService.get(key);
     if (cached) return cached;
     // **CHECK CACHE
+
+    const player = await this.player.getInfo({ player_id: player_id });
+    const agent_id = player.IdAgent;
+    const line_type_id = player.IdLineType;
 
     let _games = await this.getOpenGamesByGamesIds({
       game_ids,
@@ -166,16 +167,16 @@ export class GameService {
     const player_id = params.player_id;
     const lang_id = params.lang_id;
     const period = params.period;
-    const player = await this.player.getInfo({ player_id: player_id });
-    const agent_id = player.IdAgent;
-    const line_type_id = player.IdLineType;
 
     // **CHECK CACHE
     const key = `get_game_by_league_${league_ids}_${player_id}_${lang_id}_${period}`;
     const cached = await this.cacheService.get(key);
-
     if (cached) return cached;
     // **CHECK CACHE
+
+    const player = await this.player.getInfo({ player_id: player_id });
+    const agent_id = player.IdAgent;
+    const line_type_id = player.IdLineType;
 
     const leaguePromises = league_ids.map((league_id) =>
       this.processLeague(league_id, agent_id, line_type_id, lang_id, period),
@@ -197,16 +198,16 @@ export class GameService {
     const player_id = params.player_id;
     const lang_id = params.lang_id;
     const period = params.period;
-    const player = await this.player.getInfo({ player_id: player_id });
-    const agent_id = player.IdAgent;
-    const line_type_id = player.IdLineType;
 
     // **CHECK CACHE
     const key = `get_family_game_${family_game_id}_${player_id}_${lang_id}`;
     const cached = await this.cacheService.get(key);
-
     if (cached) return cached;
     // **CHECK CACHE
+
+    const player = await this.player.getInfo({ player_id: player_id });
+    const agent_id = player.IdAgent;
+    const line_type_id = player.IdLineType;
 
     const games = await this.getOpenGamesFamily({
       family_game_id,
@@ -220,16 +221,12 @@ export class GameService {
       family_game_id,
       line_type_id,
       lang_id,
-      // agent_id,
-      // period,
     });
 
     const optionsPROPS = await this.getGamePROPOddsByFamilyGameId({
       family_game_id,
       line_type_id,
       lang_id,
-      // agent_id,
-      // period,
     });
 
     const banners = await this.getGameBannersByFamilyGameId({
@@ -281,6 +278,14 @@ export class GameService {
     const end_date = params.end_date;
     const period = params.period;
     const league_ids = params.league_ids;
+    let webrow_ids = params.webrow_id;
+
+    // **CHECK CACHE
+    const key = `get_game_by_webrow_${webrow_ids}_${player_id}_${lang_id}`;
+    const cached = await this.cacheService.get(key);
+    if (cached) return cached;
+    // **CHECK CACHE
+
     const player = await this.player.getInfo({ player_id: player_id });
     const agent_id = player.IdAgent;
     const line_type_id = player.IdLineType;
@@ -291,17 +296,8 @@ export class GameService {
       lang_id,
       league_ids,
     });
-    const webrow_ids =
-      params.webrow_id != -1
-        ? params.webrow_id
-        : webrows.map((a: any) => a.IdWebRow);
-
-    // **CHECK CACHE
-    const key = `get_game_by_webrow_${webrow_ids}_${player_id}_${lang_id}`;
-    const cached = await this.cacheService.get(key);
-
-    if (cached) return cached;
-    // **CHECK CACHE
+    webrow_ids =
+      webrow_ids != -1 ? webrow_ids : webrows.map((a: any) => a.IdWebRow);
 
     let data: any = [];
     const webrowlength = webrow_ids.length;
@@ -435,6 +431,11 @@ export class GameService {
   }
 
   async searchGamesLeagues(params: any) {
+    const player = await this.player.getInfo({
+      player_id: params.player_id,
+    });
+    params.book_id = player.IdBook;
+    params.line_type_id = player.IdLineType;
     return {
       games: await this.searchGames(params),
       leagues: await this.searchLeagues(params),
@@ -444,10 +445,9 @@ export class GameService {
   async searchGames(params: any) {
     const cacheTimeSec = 60;
     const search = params.search;
-    const player = await this.player.getInfo({ player_id: params.player_id });
-    const book_id = player.IdBook;
-    const line_type_id = player.IdLineType;
     const lang_id = params.lang_id;
+    const book_id = params.book_id;
+    const line_type_id = params.line_type_id;
 
     // **CHECK CACHE
     const key = `search_games_${search}_${book_id}_${line_type_id}_${lang_id}`;
@@ -469,10 +469,9 @@ export class GameService {
   async searchLeagues(params: any) {
     const cacheTimeSec = 60;
     const search = params.search;
-    const player = await this.player.getInfo({ player_id: params.player_id });
-    const book_id = player.IdBook;
-    const line_type_id = player.IdLineType;
     const lang_id = params.lang_id;
+    const book_id = params.book_id;
+    const line_type_id = params.line_type_id;
 
     // **CHECK CACHE
     const key = `search_leagues_${search}_${book_id}_${line_type_id}_${lang_id}`;
@@ -656,8 +655,6 @@ export class GameService {
     const family_game_id = params.family_game_id;
     const line_type_id = params.line_type_id;
     const lang_id = params.lang_id;
-    // const agent_id = params.agent_id;
-    // const period = params.period ?? -1;
 
     // **CHECK CACHE
     const key = `get_game_tnt_odds_family_game_${family_game_id}_${line_type_id}_${lang_id}`;
@@ -680,8 +677,6 @@ export class GameService {
     const family_game_id = params.family_game_id;
     const line_type_id = params.line_type_id;
     const lang_id = params.lang_id;
-    // const agent_id = params.agent_id;
-    // const period = params.period ?? -1;
 
     // **CHECK CACHE
     const key = `get_game_prop_odds_family_game_${family_game_id}_${line_type_id}_${lang_id}`;
