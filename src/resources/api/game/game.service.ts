4b496d7374e2bc0703b8570c392b0b2188ecf65a
Alexander <desousa.alexander@gmail.com>
2025-01-09 13:48:54 -0400

    Fixed Promise in getGamesByGameIds

--- src/resources/api/game/game.service.ts
+++ src/resources/api/game/game.service.ts
@@ -77,13 +77,6 @@ export class GameService {
 
   async processLeague(league_id, agent_id, line_type_id, lang_id, period) {
     try {
-      const league_promises = [
-        this.getLeague({ league_id, lang_id }),
-        this.getLeagueBanners({ league_id, lang_id }),
-      ];
-
-      const [league, banner] = await Promise.all(league_promises);
-
       const games = await this.getOpenGamesLeague({
         league_id,
         agent_id,
@@ -96,12 +89,17 @@ export class GameService {
         return null;
       }
 
+      const league_promises = [
+        this.getLeague({ league_id, lang_id }),
+        this.getLeagueBanners({ league_id, lang_id }),
+      ];
+
+      const [league, banner] = await Promise.all(league_promises);
+
       const events = {};
 
       const gamePromises = games.map(async (game) => {
         game.banners = banner.filter((b) => b.ParentGame === game.IdGame);
-
-        const date = new Date(game.GameDate).toISOString().split('T')[0];
 
         let _main;
         if (games.some((_game) => _game.IdGame === game.FamilyGame)) {
@@ -113,28 +111,29 @@ export class GameService {
         const sport_id = game.IdSport.trim();
         const game_id = game.IdGame;
 
-        if (['TNT', 'PROP'].includes(sport_id)) {
-          let options;
-          switch (sport_id) {
-            case 'TNT':
-              options = await this.getGameTNTOddsByFamilyGameId({
+        switch (sport_id) {
+          case 'TNT':
+            game.Options = (
+              await this.getGameTNTOddsByFamilyGameId({
                 family_game_id: game_id,
                 line_type_id,
                 lang_id,
-              });
-              game.Options = options.filter((o) => o.IdGame === game_id);
-              break;
-            case 'PROP':
-              options = await this.getGamePROPOddsByFamilyGameId({
+              })
+            ).filter((o) => o.IdGame === game_id);
+            break;
+          case 'PROP':
+            game.Options = (
+              await this.getGamePROPOddsByFamilyGameId({
                 family_game_id: game_id,
                 line_type_id,
                 lang_id,
-              });
-              game.Options = options.filter((o) => o.ParentGame === game_id);
-              if (game.Options.length === 0) return;
-              break;
-          }
+              })
+            ).filter((o) => o.ParentGame === game_id);
+            if (game.Options.length === 0) return;
+            break;
         }
+
+        const date = new Date(game.GameDate).toISOString().split('T')[0];
 
         if (!events[date]) {
           events[date] = {};
