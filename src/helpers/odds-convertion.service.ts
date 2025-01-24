73099ee43cfac9dd17563c60d047ac1962b4c8fd
Alexander <desousa.alexander@gmail.com>
2025-01-24 12:51:06 -0400

    Promise in search

--- src/helpers/odds-convertion.service.ts
+++ src/helpers/odds-convertion.service.ts
@@ -19,28 +19,6 @@ export class OddsConvertionService {
   ];
   constructor() {}
 
-  OddsDecimal = {
-    toAmerican: function (decimal: any) {
-      return decimal < 2.0
-        ? (-100 / (decimal - 1)).toPrecision(5)
-        : ((decimal - 1) * 100).toPrecision(5);
-    },
-    toFractional: function (decimal: any) {
-      return math.format(math.fraction(decimal - 1), { fraction: 'ratio' });
-    },
-  };
-
-  OddsFraction = {
-    toAmerican: function (fraction: any) {
-      const [n, d] = fraction.split('/');
-      return n > d ? (n / d) * 100 : -100 / (n / d);
-    },
-    toDecimal: function (fraction: any) {
-      const [n, d] = fraction.split('/');
-      return n / d + 1;
-    },
-  };
-
   OddsAmerican = {
     toDecimal: function (moneyline: any) {
       return (
