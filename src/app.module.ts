1027e5fa09001d0cb83704edc62631612be0903c
Alexander <desousa.alexander@gmail.com>
2025-02-07 13:17:42 -0400

    UPDATE:: depuracion

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,13 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { Redis } from './config/config-redis';
-import { WebSocketModule } from './websockets/websocket.module';
-import { KafkaModule } from './microservices/kafka/kafka.module';
-import { OddsConsumer } from './odds.consumer';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
 @Module({
   imports: [
     configOptions,
     AuthModule,
-    // KafkaModule,
     ResourcesModule,
     SharedModule,
-    // WebSocketModule,
     Redis,
     ThrottlerModule.forRoot([
       {
@@ -29,7 +24,6 @@ import { TransformInterceptor } from './interceptor/transform.interceptor';
   ],
   providers: [
     ConfigModule,
-    // OddsConsumer,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
